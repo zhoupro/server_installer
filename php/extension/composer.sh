a6671040fad624faeb3cf32159da11f573920ce7
prozhou <zhoushengzheng@gmail.com>
2017-04-26 11:09:11 +0800

    fix for general use not depending on xx.net

--- php/extension/composer.sh
+++ php/extension/composer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 #install composer
-/usr/local/bin/pxy wget --no-check-certificate  "https://getcomposer.org/download/1.4.0/composer.phar" && mv composer.phar /usr/local/bin/composer
+ wget --no-check-certificate  "https://getcomposer.org/download/1.4.0/composer.phar" && mv composer.phar /usr/local/bin/composer
 chmod 777 -R /usr/local/bin/composer
 
