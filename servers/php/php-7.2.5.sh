ae5c358bf79d57a9cb3c510a159339d172eb9ea7
zhoushengzheng <zhoushengzheng@gmail.com>
2018-05-09 21:32:27 +0800

    php7.2.5

--- servers/php/php-7.2.5.sh
+++ servers/php/php-7.2.5.sh
@@ -5,11 +5,11 @@ function pre_install(){
 }
 
 function post_install(){
+
+    PHP_FPM_CONF="$BASE_DIR/server/php/etc/php-fpm.d/www.conf"
     cd ..
     cp ./php-7.2.5/php.ini-production $BASE_DIR/server/php/etc/php.ini
     #adjust php.ini
-    extension_dir="$BASE_DIR/server/php/bin/php-config --extension-dir"
-    sed -i 's#; extension_dir = \"\.\/\"#extension_dir = "'$extension_dir'"#'  $BASE_DIR/server/php/etc/php.ini
     sed -i 's/post_max_size = 8M/post_max_size = 64M/g' $BASE_DIR/server/php/etc/php.ini
     sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 64M/g' $BASE_DIR/server/php/etc/php.ini
     sed -i 's/;date.timezone =/date.timezone = PRC/g' $BASE_DIR/server/php/etc/php.ini
@@ -17,20 +17,20 @@ function post_install(){
     sed -i 's/max_execution_time = 30/max_execution_time = 300/g' $BASE_DIR/server/php/etc/php.ini
     #adjust php-fpm
     cp $BASE_DIR/server/php/etc/php-fpm.conf.default $BASE_DIR/server/php/etc/php-fpm.conf
-    sed -i 's,user = nobody,user=www,g'   $BASE_DIR/server/php/etc/php-fpm.conf
-    sed -i 's,group = nobody,group=www,g'   $BASE_DIR/server/php/etc/php-fpm.conf
-    sed -i 's,^pm.min_spare_servers = 1,pm.min_spare_servers = 5,g'   $BASE_DIR/server/php/etc/php-fpm.conf
-    sed -i 's,^pm.max_spare_servers = 3,pm.max_spare_servers = 35,g'   $BASE_DIR/server/php/etc/php-fpm.conf
-    sed -i 's,^pm.max_children = 5,pm.max_children = 100,g'   $BASE_DIR/server/php/etc/php-fpm.conf
-    sed -i 's,^pm.start_servers = 2,pm.start_servers = 20,g'   $BASE_DIR/server/php/etc/php-fpm.conf
-    sed -i 's,;pid = run/php-fpm.pid,pid = run/php-fpm.pid,g'   $BASE_DIR/server/php/etc/php-fpm.conf
-    sed -i 's,listen = 127.0.0.1:9000,listen = [::]:9000,g'   $BASE_DIR/server/php/etc/php-fpm.conf
-    sed -i 's,;error_log = log/php-fpm.log,error_log = '$BASE_DIR'/server/php/var/log/php-fpm.log,g'   $BASE_DIR/server/php/etc/php-fpm.conf
-    sed -i 's,;slowlog = log/$pool.log.slow,slowlog = '$BASE_DIR'/server/php/var/log/\$pool.log.slow,g'   $BASE_DIR/server/php/etc/php-fpm.conf
     #self start
-    cp $BASE_DIR/server/php/etc/php-fpm.d/www.conf.default $BASE_DIR/server/php/etc/php-fpm.d/www.conf
-    sed -i 's,listen = 127.0.0.1:9000,listen = 9000,g' $BASE_DIR/server/php/etc/php-fpm.d/www.conf
-    install -v -m755 ./php-7.1.3/sapi/fpm/init.d.php-fpm  /etc/init.d/php-fpm
+    cp $BASE_DIR/server/php/etc/php-fpm.d/www.conf.default $PHP_FPM_CONF 
+    sed -i 's,listen = 127.0.0.1:9000,listen = 9000,g' $PHP_FPM_CONF 
+    sed -i 's,user = nobody,user=www,g' $PHP_FPM_CONF 
+    sed -i 's,group = nobody,group=www,g' $PHP_FPM_CONF 
+    sed -i 's,^pm.min_spare_servers = 1,pm.min_spare_servers = 5,g'  $PHP_FPM_CONF 
+    sed -i 's,^pm.max_spare_servers = 3,pm.max_spare_servers = 35,g'  $PHP_FPM_CONF 
+    sed -i 's,^pm.max_children = 5,pm.max_children = 100,g' $PHP_FPM_CONF 
+    sed -i 's,^pm.start_servers = 2,pm.start_servers = 20,g' $PHP_FPM_CONF 
+    sed -i 's,;pid = run/php-fpm.pid,pid = run/php-fpm.pid,g'  $PHP_FPM_CONF 
+    sed -i 's,listen = 127.0.0.1:9000,listen = [::]:9000,g'  $PHP_FPM_CONF 
+    sed -i 's,;error_log = log/php-fpm.log,error_log = '$BASE_DIR'/server/php/var/log/php-fpm.log,g' $PHP_FPM_CONF 
+    sed -i 's,;slowlog = log/$pool.log.slow,slowlog = '$BASE_DIR'/server/php/var/log/\$pool.log.slow,g' $PHP_FPM_CONF 
+    install -v -m755 ./php-7.2.5/sapi/fpm/init.d.php-fpm  /etc/init.d/php-fpm
     /etc/init.d/php-fpm start
 }
 
@@ -41,8 +41,14 @@ function install_server(){
     fi
     tar zxvf php-7.2.5.tar.gz
     cd php-7.2.5
+    
+    if [ "x$SERVER_DEBUG" == "x1" ]
+    then
+        CONFIG_DEBUG="--with-debug"
+    fi
     ./configure --prefix=$BASE_DIR/server/php \
     --with-config-file-path=$BASE_DIR/server/php/etc \
+    $CONFIG_DEBUG \
     --with-mysqli=mysqlnd \
     --with-pdo-mysql=mysqlnd \
     --enable-fpm \
