38a6328bbfa4e20b224178257a9e16ed3d56312d
zhoushengzheng <zhoushengzheng@didichuxing.com>
2018-05-13 00:14:19 +0800

    test

--- env/ubuntu_dep.sh
+++ env/ubuntu_dep.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-sudo apt-get install automake m4 autoconf libtool build-essential -y
+apt-get update
+apt-get install sudo git wget curl -y 
+sudo apt-get install automake m4 autoconf libtool build-essential cmake -y
