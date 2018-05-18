7f8dca03b5c46a4a1805177139094557229eaf60
zhoushengzheng <zhoushengzheng@didichuxing.com>
2018-05-18 10:25:08 +0800

    dep

--- servers/php/dep_env.sh
+++ servers/php/dep_env.sh
@@ -3,4 +3,4 @@ useradd www
 sudo apt-get install -y  libxml2 libxml2-dev \
     libbz2-dev  libjpeg-dev libpng-dev \
     libxpm-dev libfreetype6-dev  libmcrypt-dev \
-    libmysql++-dev  libxslt1-dev openssl libssl-dev  libcurl4-openssl-dev libsassl2-dev pkg-config
+    libmysql++-dev  libxslt1-dev openssl libssl-dev  libcurl4-openssl-dev pkg-config
