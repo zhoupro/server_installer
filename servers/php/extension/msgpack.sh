ec26413b6e9f01985e02e70a4dcdb0319d4856bc
prozhou <zhoushengzheng@gmail.com>
2018-10-24 08:50:17 +0000

    fix

--- servers/php/extension/msgpack.sh
+++ servers/php/extension/msgpack.sh
@@ -5,12 +5,12 @@ case $php_version in
         git clone https://github.com/msgpack/msgpack-php.git 
         cd msgpack-php 
         git checkout msgpack-2.0.2 
-        $install_dir/server/php/bin/phpize
-        ./configure  --with-php-config=$install_dir/server/php/bin/php-config
+        $BASE_DIR/server/php/bin/phpize
+        ./configure  --with-php-config=$BASE_DIR/server/php/bin/php-config
         make
         make install
         cd ..
-        echo "extension=msgpack.so" >> $install_dir/server/php/etc/php.ini
+        echo "extension=msgpack.so" >> $BASE_DIR/server/php/etc/php.ini
         ;;
 esac
 
