3e6752561762d532471df0554041887cd12fd33b
prozhou <zhoushengzheng@gmail.com>
2019-06-18 19:36:22 +0800

    node

--- servers/node/node-ubuntu.sh
+++ servers/node/node-ubuntu.sh
@@ -15,12 +15,15 @@ function install_server(){
     fi
 
     if [ ! -f  node-v${SERVER_VERSION}-linux-x64.tar.xz ];then
-        mkdir -p /usr/local/lib/nodejs
-        wget https://nodejs.org/dist/v${SERVER_VERSION}/node-v${SERVER_VERSION}-linux-x64.tar.xz
-        tar -C /usr/local/lib/nodejs -xJf   node-v${SERVER_VERSION}-linux-x64.tar.xz
+        mkdir -p /usr/local/lib/nodejs && \
+        wget https://nodejs.org/dist/v${SERVER_VERSION}/node-v${SERVER_VERSION}-linux-x64.tar.xz && \
+        tar -C /usr/local/lib/nodejs -xJf   node-v${SERVER_VERSION}-linux-x64.tar.xz && \
+        mv /usr/local/lib/nodejs/node-v${SERVER_VERSION}-linux-x64  /usr/local/lib/nodejs/node
     fi
-    export PATH=$PATH:/usr/local/lib/nodejs/node-v${SERVER_VERSION}-linux-x64/bin
-    echo "export PATH=\$PATH:/usr/local/lib/nodejs/node-v${SERVER_VERSION}-linux-x64/bin" >> ~/.env
+    ln -s  /usr/local/lib/nodejs/node/bin/npm /usr/local/bin/npm
+    ln -s  /usr/local/lib/nodejs/node/bin/node /usr/local/bin/node
+    export PATH=$PATH:/usr/local/lib/nodejs/node/bin
+    echo "export PATH=\$PATH:/usr/local/lib/nodejs/node/bin" >> ~/.env
 }
 function remove_server(){
     echo "removing node"
